c510d2e1f52299882e449de5edd8f5cf7aabb0b2
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-08-13 14:29:18 +0900

    docs: change href

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/Sangyeonglee353/morethan-log",
     },
   ],
   // blog setting (required)
